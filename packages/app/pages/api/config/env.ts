5df84f7daa3216c6ac224fa612ada67e27a1c44d
benrobo <alumonabenaiah71@gmail.com>
2023-06-22 12:06:29 +0100

    started auth

--- packages/app/pages/api/config/env.ts
+++ packages/app/pages/api/config/env.ts
@@ -1,9 +1,11 @@
-
 const LOCAL_DB_CONN = "mongodb://localhost:27020/prospark-db";
 
 const ENV = {
   jwtSecret: process.env.JWT_SECRET,
-  mongoUrl: process.env.NODE_ENV === "development" ? LOCAL_DB_CONN : process.env.MONGODB,
+  mongoUrl:
+    process.env.NODE_ENV === "development"
+      ? LOCAL_DB_CONN
+      : process.env.DATABASE_URL,
   clientUrl:
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
--- packages/app/pages/auth/index.tsx
+++ packages/app/pages/auth/index.tsx
@@ -1,7 +1,16 @@
-export default function Auth() {
+import { useEffect } from "react";
+
+function Login() {
+  useEffect(() => {
+    require("@passageidentity/passage-elements/passage-auth");
+  }, []);
+
   return (
-    <div>
-      <p className="">Auth Page</p>
+    <div className="w-full h-screen flex flex-col items-center justify-center bg-white-100 ">
+      <div className="w-full max-w-[400px] bg-white-300 border-solid border-[2px] border-white-600 pp-SB ">
+        <passage-auth app-id="Wk5nQ0hQ9KfcNaTv5kZlAkWt"></passage-auth>
+      </div>
     </div>
   );
 }
+export default Login;
