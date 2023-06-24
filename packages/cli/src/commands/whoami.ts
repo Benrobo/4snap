31e1f44f95b239005db318cc822a61f0f79a5a5f
benrobo <alumonabenaiah71@gmail.com>
2023-06-24 18:29:12 +0100

    added command execution with dynamic variable

--- packages/cli/src/commands/whoami.ts
+++ packages/cli/src/commands/whoami.ts
@@ -4,7 +4,7 @@ import Table from "cli-table";
 import chalk from "chalk";
 
 export default async function whoami() {
-  intro(chalk.bgBlueBright(chalk.whiteBright(" Qwik user information ")));
+  intro(chalk.bgBlueBright(chalk.whiteBright(" 4snap user information ")));
   const userData = storage.get("@userInfo") as any;
 
   if (typeof userData === "undefined" || userData === null) {
