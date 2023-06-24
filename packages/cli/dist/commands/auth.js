a853a214bd1059590cc47d5c44daff0fae108f71
benrobo <alumonabenaiah71@gmail.com>
2023-06-24 09:32:34 +0100

    added cli authentication

--- packages/cli/dist/commands/auth.js
+++ packages/cli/dist/commands/auth.js
@@ -2,31 +2,40 @@ import { text, intro } from "@clack/prompts";
 import showLoading from "../helpers/loader.js";
 import { authenticate } from "../helpers/http.js";
 import { checkInvalidToken, checkServerError, } from "../helpers/serverResponse.js";
-export async function authCliApp(token) {
+import Conf from "conf";
+const storage = new Conf({ projectName: "qwik" });
+export async function authCliApp() {
     intro("qwik");
+    const spinner = await showLoading();
     try {
-        const LOADING = await showLoading();
         const userToken = await text({
-            message: "Qwik Token",
+            message: "Enter your qwik token: ",
             placeholder: "xxxxxxxxx",
             validate(value) {
                 if (value.length === 0)
                     return `Value is required!`;
             },
         });
-        LOADING.start("Authenticating..");
+        spinner.start("Authenticating..");
         const resp = await authenticate({ token: userToken });
         if (["--cliAuth/invalid-token"].includes(resp?.code)) {
-            LOADING.stop(null, resp?.message);
+            spinner.fail(resp?.message);
         }
         if (["--cliAuth/success"].includes(resp?.code)) {
-            LOADING.stop(resp?.message, null);
-            console.log(resp.data);
+            spinner.success(resp?.message);
+            storage.set("@authToken", resp?.data?.token);
+            storage.set("@userInfo", {
+                username: resp?.data?.username,
+                email: resp?.data?.email,
+                uId: resp?.data?.userId,
+            });
+            process.exit();
         }
+        spinner.stop();
         checkServerError(resp);
         checkInvalidToken(resp);
     }
     catch (e) {
-        console.log(`Error authenticating user: ${e.message}`);
+        spinner.fail(`Failed to authenticate, Try again later.`);
     }
 }
