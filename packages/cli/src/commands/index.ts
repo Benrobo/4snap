db93e625de58a01eca3067af1bf9f37c1e383a71
benrobo <alumonabenaiah71@gmail.com>
2023-06-25 04:18:12 +0100

    completed command sharing via cli

--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -4,3 +4,4 @@ export { default as createCmd } from "./createCmd.js";
 export { default as listCommands } from "./listCmds.js";
 export { default as execCmd } from "./executeCmd.js";
 export { default as syncCmd } from "./syncCmd.js";
+export { default as shareCmd } from "./shareCmd.js";
