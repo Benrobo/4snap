ba3f9714916685d136fb7a86030596f83f19e17e
benrobo <alumonabenaiah71@gmail.com>
2023-06-26 21:07:22 +0100

    added viewing of command

--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -5,3 +5,4 @@ export { default as listCommands } from "./listCmds.js";
 export { default as execCmd } from "./executeCmd.js";
 export { default as syncCmd } from "./syncCmd.js";
 export { default as shareCmd } from "./shareCmd.js";
+export { default as viewCmd } from "./view.js";
