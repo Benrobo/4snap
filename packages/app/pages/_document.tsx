abfda3dd2e65fad30352d0b914ce052326845b07
benrobo <alumonabenaiah71@gmail.com>
2023-06-27 14:01:31 +0100

    updated

--- packages/app/pages/_document.tsx
+++ packages/app/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="twitter:image" content="/logo.svg" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="4snap" />
+        <meta property="og:image" content="/screenshots/4snap-bg.png" />
+      </Head>
+      <title>4snap</title>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
