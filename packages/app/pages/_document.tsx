16c44175f99500cd5eb81d5d01545cc8242d09a5
benrobo <alumonabenaiah71@gmail.com>
2023-06-27 14:03:43 +0100

    updated meta tags

--- packages/app/pages/_document.tsx
+++ packages/app/pages/_document.tsx
@@ -4,10 +4,16 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta property="twitter:image" content="/logo.svg" />
+        <meta
+          property="twitter:image"
+          content="https://raw.githubusercontent.com/Benrobo/4snap/main/packages/app/public/screenshots/4snap-bg.png"
+        />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:title" content="4snap" />
-        <meta property="og:image" content="/screenshots/4snap-bg.png" />
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/Benrobo/4snap/main/packages/app/public/screenshots/4snap-bg.png"
+        />
       </Head>
       <title>4snap</title>
       <body>
